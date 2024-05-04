e6f50343093502869c367cc23252b053558d0a61
Renato Apaza T <renato.apaza@outlook.com>
2024-05-03 20:26:08 -0400

    Terraform ejemplo 1

---
+++ lab/terraform/01.- ec2/main.tf
@@ -0,0 +1,27 @@
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.47.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+  # profile = "default"
+}
+
+resource "aws_instance" "myec2" {
+  ami           = "ami-07caf09b362be10b8" #Imagen del SO amazon
+  instance_type = "t2.micro"
+  key_name      = "devops-udi-2024"
+  tags = {
+    Name = "tf-rapaza"
+  }
+}
+
+resource "aws_s3_bucket" "my_s3_bucket" {
+  bucket = "tf-s3-rapaza"  #debe ser nombre unico
+}
