1ca34acb339c1c445cd65b190ccd4bf6acadbccf
Renato Apaza T <renato.apaza@outlook.com>
2024-05-06 20:07:04 -0400

    laboratorio

--- lab/terraform/01.- ec2/main.tf
+++ lab/terraform/01.- ec2/main.tf
@@ -17,10 +17,13 @@ resource "aws_instance" "myec2" {
   ami           = "ami-07caf09b362be10b8" #Imagen del SO amazon
   instance_type = "t2.micro"
   key_name      = "devops-udi-2024"
+  count = 4
   tags = {
     Name = "tf-rapaza"
   }
 }
+
+
 
 resource "aws_s3_bucket" "my_s3_bucket" {
   bucket = "tf-s3-rapaza"  #debe ser nombre unico
